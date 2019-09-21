71304ea1cd8310865d9bf06f0202f615378b8237
Tyler Nichols <tegulator98@gmail.com>
2019-09-21 10:41:24 -0600

    trying to fix scraping

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require ("express");
 var handlebars = require("express-handlebars");
 var mongoose = require("mongoose");
-var cheerio = require("cherrio");
+var cheerio = require("cheerio");
 var axios = require("axios");
+var db = require("./modules");
 var PORT = 3000;
 
 axios.get("https://www.nytimes.com/").then(function(response) {
@@ -13,38 +14,31 @@ axios.get("https://www.nytimes.com/").then(function(response) {
   
   var results = [];
 
-  $("#div.css-1qj0wac eqveam61").each(function(i, element) {
+  $("h2.css-1qwxefa").each(function(i, element) {
 
     
-    var title = $("h2").text();
-
-    var summary = $("p").text();
-
-    
-    var link = $(element).children().attr("href");
+    var title = $(element).children("span").text();
 
     results.push({
       title: title,
-      link: link,
-      summary: summary
     });
-    console.log(results);
-  });
+});
+console.log(results);
 });
 
-mongoose.connect("mongodb://localhost/NYTimes", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/NYTimes", { useNewUrlParser: true });
 
 
-db.Library.create({ name: "NYT" })
-  .then(function(dbLibrary) {
+// db.Library.create({ name: "NYT" })
+//   .then(function(dbLibrary) {
    
-    console.log(dbLibrary);
-  })
-  .catch(function(err) {
+//     console.log(dbLibrary);
+//   })
+//   .catch(function(err) {
     
-    console.log(err.message);
-  });
+//     console.log(err.message);
+//   });
 
-  app.listen(PORT, function() {
-    console.log("App running on port http://" + PORT + "!");
-  });
+//   app.listen(PORT, function() {
+//     console.log("App running on port http://" + PORT + "!");
+//   });
