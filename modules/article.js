bce0897e2c13006d2a984cf91da4ed33101c1a9c
Tyler Nichols <tegulator98@gmail.com>
2019-09-21 13:46:03 -0600

    Server cleaned up and better and Article improved

--- modules/article.js
+++ modules/article.js
@@ -1,16 +1,47 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
+// Save a reference to the Schema 
+const Schema = mongoose.Schema;
 
-var Schema = mongoose.Schema;
-
-
-var ArticleSchema = new Schema({
-  title: String,
-  summary: String
+// Using the constructor, create a new Schema object
+const ArticleSchema = new Schema({
+  // `headline` is required and of type String
+  headline: {
+    type: String,
+    required: true
+  },
+  // `url` is required and of type String
+  url: {
+    type: String,
+    required: true
+  },
+  // `summary` is required and of type String
+  summary: {
+    type: String,
+    required: true
+  },
+  // `save` is required and of type String
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  picture: {
+    type: String
+  },
+  
+  // This allows us to populate the Article with an associated Note
+  note: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
 });
 
+// This creates our model from the above schema, using mongoose's model method
+const Article = mongoose.model("Article", ArticleSchema);
 
-var Article = mongoose.model("Article", ArticleSchema);
-
-
+// Export the Article model
 module.exports = Article;
--- server.js
+++ server.js
@@ -1,44 +1,38 @@
-var express = require ("express");
-var handlebars = require("express-handlebars");
-var mongoose = require("mongoose");
-var cheerio = require("cheerio");
-var axios = require("axios");
-var db = require("./modules");
-var PORT = 3000;
+// Requires
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-axios.get("https://www.nytimes.com/").then(function(response) {
+const PORT = process.env.PORT || 3000;
 
-  
-  var $ = cheerio.load(response.data);
+const app = express();
 
-  
-  var results = [];
-
-  $("h2.css-1qwxefa").each(function(i, element) {
-
-    
-    var title = $(element).children("span").text();
-
-    results.push({
-      title: title,
-    });
-});
-console.log(results);
-});
-
-// mongoose.connect("mongodb://localhost/NYTimes", { useNewUrlParser: true });
+// Middle
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+// Connection to Mongo
+let dbconnect = mongoose.connection;
+dbconnect.on("error", console.error.bind(console, "connection error:"));
+dbconnect.once("open", function () {
+    console.log("Connected to Mongoose");
+})
 
 
-// db.Library.create({ name: "NYT" })
-//   .then(function(dbLibrary) {
-   
-//     console.log(dbLibrary);
-//   })
-//   .catch(function(err) {
-    
-//     console.log(err.message);
-//   });
+// Handlebars.
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-//   app.listen(PORT, function() {
-//     console.log("App running on port http://" + PORT + "!");
-//   });
+// Routes
+require("./routes/api_routes")(app);
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
