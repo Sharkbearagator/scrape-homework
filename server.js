a0c3d4f5a2323f5fb4d5766da794362f2837b0e2
Tyler Nichols <tegulator98@gmail.com>
2019-09-21 09:38:34 -0600

    adding server and git ignore

--- server.js
+++ server.js
@@ -3,3 +3,48 @@ var handlebars = require("express-handlebars");
 var mongoose = require("mongoose");
 var cheerio = require("cherrio");
 var axios = require("axios");
+var PORT = 3000;
+
+axios.get("https://www.nytimes.com/").then(function(response) {
+
+  
+  var $ = cheerio.load(response.data);
+
+  
+  var results = [];
+
+  $("#div.css-1qj0wac eqveam61").each(function(i, element) {
+
+    
+    var title = $("h2").text();
+
+    var summary = $("p").text();
+
+    
+    var link = $(element).children().attr("href");
+
+    results.push({
+      title: title,
+      link: link,
+      summary: summary
+    });
+    console.log(results);
+  });
+});
+
+mongoose.connect("mongodb://localhost/NYTimes", { useNewUrlParser: true });
+
+
+db.Library.create({ name: "NYT" })
+  .then(function(dbLibrary) {
+   
+    console.log(dbLibrary);
+  })
+  .catch(function(err) {
+    
+    console.log(err.message);
+  });
+
+  app.listen(PORT, function() {
+    console.log("App running on port http://" + PORT + "!");
+  });
